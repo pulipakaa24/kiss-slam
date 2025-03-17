c1ee34d5de05081c78f4263a781b6ab639c48a64
ssg1002 <sgupta2@uni-bonn.de>
2025-03-17 17:42:30 +0100

    fix cibuildwheels thru cmake args in pyproject

--- kiss_slam/kiss_slam_pybind/3rdparty/find_dependencies.cmake
+++ kiss_slam/kiss_slam_pybind/3rdparty/find_dependencies.cmake
@@ -36,9 +36,8 @@ function(find_external_dependency PACKAGE_NAME TARGET_NAME INCLUDED_CMAKE_PATH)
   endif()
 endfunction()
 
-find_external_dependency("Eigen3" "Eigen3::Eigen" "${CMAKE_CURRENT_LIST_DIR}/eigen/eigen.cmake")
 find_external_dependency("tsl-robin-map" "tsl::robin_map" "${CMAKE_CURRENT_LIST_DIR}/tsl_robin/tsl_robin.cmake")
+find_external_dependency("Eigen3" "Eigen3::Eigen" "${CMAKE_CURRENT_LIST_DIR}/eigen/eigen.cmake")
+include(${CMAKE_CURRENT_LIST_DIR}/bonxai/bonxai.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/suitesparse/suitesparse.cmake)
 find_external_dependency("g2o" "g2o::core" "${CMAKE_CURRENT_LIST_DIR}/g2o/g2o.cmake")
-
-include(${CMAKE_CURRENT_LIST_DIR}/bonxai/bonxai.cmake)
